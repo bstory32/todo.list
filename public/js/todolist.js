7c6f63e7374411979863a0e915e360293fc75dba
Brian Story <bstory32@gmail.com>
2014-04-08 15:38:44 -0500

    added todo list javascript file

---
+++ public/js/todolist.js
@@ -0,0 +1,21 @@
+var $listData = $('[context="list-data"]');
+		// Get Data
+		var array = JSON.parse(localStorage.getItem('listData')) || [];
+		
+		if (array == '') {
+			console.log('none');
+			$listData.append('<li></li>')
+		};
+		for (var i = 0; i < array.length; i++) {
+			$listData.append('<li>' + array[i] + '</li>')
+		};
+
+	  	// store data
+	  	$listData.on('blur paste input', function(){
+	  		var array = [];
+	  		$listData.find('li').each(function(){
+	  			array.push($(this).text())
+	  			localStorage.setItem('listData', JSON.stringify(array))
+	  		})
+
+	  	});
